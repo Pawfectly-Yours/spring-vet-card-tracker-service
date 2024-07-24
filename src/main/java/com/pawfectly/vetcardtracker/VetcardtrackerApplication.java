4f31312f17bcc457d99eb2ba05688015f8f3be8e
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2024-07-24 10:12:40 +0800

    first commit

---
+++ src/main/java/com/pawfectly/vetcardtracker/VetcardtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.pawfectly.vetcardtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetcardtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetcardtrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pawfectly/vetcardtracker/VetcardtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawfectly.vetcardtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetcardtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
